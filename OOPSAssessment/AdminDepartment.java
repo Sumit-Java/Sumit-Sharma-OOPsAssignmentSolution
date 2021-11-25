0d3d9b76c5be4e30718bd4154c03abd170c6b39c
Sumit Sharma <connectsecureworld@gmail.com>
2021-11-25 16:36:06 +0530

    Correction Done in Admin Class

--- OOPSAssessment/AdminDepartment.java
+++ OOPSAssessment/AdminDepartment.java
@@ -6,11 +6,11 @@ public class AdminDepartment extends SuperDepartment{
 		return "Admin Department";
 	}
 public String getTodaysWork()
-{
+	{
 	return "Complete your documents Submission";
-}
+	}
 public String getWorkDeadline()
-{
+	{
 	return "Complete by EOD";
-}
+	}
 }
